9e35eed82dfc7e726c6368dea2c557b9b806f24e
Nguyen Hai Bay <bayn2k6@gmail.com>
2026-05-11 10:50:34 +0700

    viet va test api AdminRejectProduct

--- src/test/java/com/e_cormerce/shoppe/controller/client/shoppingcart/AddItemTest.java
+++ src/test/java/com/e_cormerce/shoppe/controller/client/shoppingcart/AddItemTest.java
@@ -15,12 +15,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc(addFilters = false)
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@ActiveProfiles("test")
 public class AddItemTest {
 
     @Autowired
